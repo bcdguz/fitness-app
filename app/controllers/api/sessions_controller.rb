04e6bf3a6d9272554607eb9d2321cdb410835de2
Bryce DeGuzman <bryce@x-claim.com>
2022-09-08 17:15:57 -0700

    Add view to sessions login

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,8 +2,10 @@ class Api::SessionsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-      @user = User.find_by_credentials(params[:user][:username],
-      params[:user][:password])
+      @user = User.find_by_credentials(
+        params[:user][:username],
+        params[:user][:password]
+      )
       if !@user
           render json: ['Incorrect username or password!'], status: 401
       else    
